71a23eea975c7abd222f551f922165d2ba39a7b3
Sungjae Shin <67855888+SungJaeShin@users.noreply.github.com>
2024-02-16 01:56:06 +0900

    Marching Cubes

--- example/viz_ply.py
+++ example/viz_ply.py
@@ -1,21 +1,21 @@
 # ============= open3d version =============
-from open3d import *    
+# from open3d import *    
 
-def main():
-    ply_path = "/home/sj/workspace/catkin_ws/src/marching_cubes/example/output/marching_cubes_airplane_200.ply" 
-    cloud = io.read_point_cloud(ply_path) # Read point cloud
-    visualization.draw_geometries([cloud])    # Visualize point cloud      
+# def main():
+#     ply_path = "/home/sj/workspace/catkin_ws/src/marching_cubes/example/output/marching_cubes_airplane_50.ply" 
+#     cloud = io.read_point_cloud(ply_path) # Read point cloud
+#     visualization.draw_geometries([cloud])    # Visualize point cloud      
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
 
 # ============= pyntcloud version =============
-# from pyntcloud import PyntCloud
-# import matplotlib
+from pyntcloud import PyntCloud
+import matplotlib
 
-#ply_path = "/home/sj/workspace/catkin_ws/src/marching_cubes/example/output/marching_cubes_airplane_200.ply" 
-#cloud = PyntCloud.from_file(ply_path)
+ply_path = "/home/sj/workspace/catkin_ws/src/marching_cubes/example/output/marching_cubes.ply" 
+cloud = PyntCloud.from_file(ply_path)
 
-# cloud.plot(mesh=True, backend="matplotlib")
-#cloud.plot(backend="matplotlib", mesh=True, cmap="hsv")
+cloud.plot(mesh=True, backend="matplotlib")
+cloud.plot(backend="matplotlib", mesh=True, cmap="hsv")
